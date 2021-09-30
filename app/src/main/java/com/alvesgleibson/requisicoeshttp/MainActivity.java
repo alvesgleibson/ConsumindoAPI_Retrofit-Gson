dcc1292aa57f7574960abae86164657633e231a1
Gleibson Alves <alvesgleibson@gmail.com>
2021-09-30 19:02:00 -0300

    Consume Rest with Retrofit API + Gson

--- app/src/main/java/com/alvesgleibson/requisicoeshttp/MainActivity.java
+++ app/src/main/java/com/alvesgleibson/requisicoeshttp/MainActivity.java
@@ -2,26 +2,23 @@ package com.alvesgleibson.requisicoeshttp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
+import com.alvesgleibson.requisicoeshttp.api.CEPService;
 import com.alvesgleibson.requisicoeshttp.model.Endereco;
 import com.google.android.material.textfield.TextInputEditText;
 
-import org.json.JSONException;
-import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -29,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     private Button botaoRecuperar;
     private TextView txtResultado;
     private TextInputEditText editText;
+    private Retrofit retrofit;
 
 
 
@@ -39,100 +37,50 @@ public class MainActivity extends AppCompatActivity {
 
         botaoRecuperar = findViewById(R.id.btRecuperar);
         txtResultado = findViewById(R.id.txtResultado);
-        editText = findViewById(R.id.imputText);
+
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://viacep.com.br/ws/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
 
         botaoRecuperar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                MyTask task = new MyTask();
-                String cep = editText.getText().toString();
-                //String urlApi = "https://blockchain.info/ticker";
-                String urlCEP = "https://viacep.com.br/ws/"+cep+"/json/";
-                task.execute(urlCEP);
+                recuperarCEPRetrofit();
 
             }
         });
 
     }
 
-    class MyTask extends AsyncTask<String, Void, String> {
+    public void recuperarCEPRetrofit(){
+
+        editText = findViewById(R.id.imputText);
+        String CEP = editText.getText().toString();
 
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
+        CEPService service = retrofit.create(CEPService.class);
+        Call<Endereco> call = service.recuperarCEP(CEP);
 
-        @Override
-        protected String doInBackground(String... strings) {
+        call.enqueue(new Callback<Endereco>() {
+            @Override
+            public void onResponse(Call<Endereco> call, Response<Endereco> response) {
 
-            String stringUrl = strings[0];
-            InputStream inputStream =  null;
-            InputStreamReader streamReader = null;
-            StringBuffer buffer = null;
-            try {
-                URL url = new URL(stringUrl);
-                HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+                txtResultado.setText(response.body().toString());
 
-                // Recupera os dados em Bytes
-               inputStream =  conexao.getInputStream();
-
-               streamReader = new InputStreamReader(inputStream);
-               BufferedReader reader = new BufferedReader(streamReader);
-               buffer = new StringBuffer();
-               String linha = "";
-               while ((linha = reader.readLine()) != null){
-                   buffer.append(linha);
-               }
-
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
             }
 
-            return buffer.toString();
-        }
+            @Override
+            public void onFailure(Call<Endereco> call, Throwable t) {
 
-        @Override
-        protected void onPostExecute(String s) {
-            super.onPostExecute(s);
-
-
-            //String valorRealBit =  null;
-            //String sifraoReal = null;
-            Endereco endereco = null;
-
-            try {
-
-                /*
-                String valorObjeto = null;
-
-                JSONObject jsonObject = new JSONObject(s);
-                valorObjeto = jsonObject.getString("BRL");
-
-                JSONObject jsonObjectReal = new JSONObject(valorObjeto);
-                valorRealBit = jsonObjectReal.getString("last");
-                sifraoReal = jsonObjectReal.getString("symbol") + " "+ valorRealBit;
-                */
-
-
-                JSONObject jsonObject = new JSONObject(s);
-                String rua, cep, bairro,uf, cidade;
-                rua = jsonObject.getString("logradouro");
-                cep = jsonObject.getString("cep");
-                bairro = jsonObject.getString("bairro");
-                uf = jsonObject.getString("uf");
-                cidade = jsonObject.getString("localidade");
-                endereco = new Endereco(rua,cep,bairro,uf, cidade);
-
-
-            } catch (JSONException e) {
-                e.printStackTrace();
             }
-            txtResultado.setText(endereco.toString());
-        }
+        });
+
     }
+
+
+
 }
